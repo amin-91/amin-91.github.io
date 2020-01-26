467ab69fa9e22a95c52f567cea58940eac9a03ef
amin-91 <amin.bc29@gmail.com>
2020-01-26 10:03:44 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function quitter()
+{document.getElementById('popp').style.display = 'none'};
+function exec()
+{window.location.href ='https://www.youtube.com/watch?v=dQw4w9WgXcQ'};
+setTimeout(function()
+{document.getElementById('popp').style.display = 'block'},1000);
