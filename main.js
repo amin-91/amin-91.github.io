079beeceff8c336bec8bbb7b3ab78e9b013469a2
amin-91 <51759326+amin-91@users.noreply.github.com>
2020-01-26 19:06:03 +0100

    finally

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ function quitter()
 function exec()
 {window.location.href ='https://www.youtube.com/watch?v=dQw4w9WgXcQ'};
 setTimeout(function()
-{document.getElementById('popp').style.display = 'block'},1000);
+{document.getElementById('popp').style.display = 'block'},10000);
